47d09d1d5ff1110c1bbdc3349e68b0644fa57d70
Gemicat <gemicat@163.com>
2017-05-30 16:12:10 +0800

    新增导航组件

---
+++ src/components/common/NavMenuItem.vue
@@ -0,0 +1,258 @@
+<template>
+  <li class="m-i" :class="classes">
+    <a class="i-link" href="//www.bilibili.com/video/douga.html">
+      <em>{{item.title}}</em>
+      <div class="v-num" v-if="showNum">
+        <span class="addnew_1">{{item.num}}</span>
+      </div>
+    </a>
+    <ul class="i_num">
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      classes: {
+        home: this.item.home,
+        'm-i-square': this.item.sequare,
+        'm-i-live': this.item.live,
+        'm-i-blackroom': this.item.blackroom
+      }
+    }
+  },
+  props: {
+    item: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    showNum() {
+      if (this.item.home || this.item.sequare || this.item.live || this.item.blackroom) {
+        return false
+      }
+      return true
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+  .header .num
+
+    ul.nav-menu
+      zoom 1
+      display inline-block
+      vertical-align top
+      height 50px
+
+      &:after
+        content ""
+        display block
+        visibility hidden
+        height 0
+        clear both
+        font-size 0
+
+    li.m-i
+      float left
+      position relative
+      font-size 14px
+      display block
+      height 50px
+      margin-right -1px
+
+      &:hover .i_num
+        display block
+
+      a
+        color #222
+        display block
+
+        &:hover
+          color #00a1d6
+
+      .i-link
+        position relative
+        z-index 10
+        font-size 14px
+
+        em
+          padding-right 0
+          padding-left 0
+          margin 0
+          line-height 50px
+          display inline-block
+          vertical-align middle
+          line-height 48px
+          margin 0 10px
+          height 40px
+          padding-top 8px
+          font-style normal
+
+      .v-num
+        position absolute
+        top 8px
+        left 0
+        height 14px
+        width 100%
+        text-align center
+
+        span
+          position static
+          display inline-block
+          vertical-align top
+          top auto
+          left auto
+          margin-left 0
+          background-color #ffafc9
+          line-height 12px
+      span
+        position absolute
+        top 0
+        left 50%
+        margin-left -12px
+        color #fff
+        border-radius 4px
+        height 12px
+        line-height 10px
+        text-align center
+        font-family Calibri,Arial,Helvetica,sans-serifsans-serif
+        background-color #ffafc9
+        font-size 11px
+        min-width 18px
+        padding 1px 3px
+
+    li.m-i.m-i-square
+      margin 0 16px 0 10px
+
+      a.i-link
+        margin 0
+
+        em
+          padding-left 22px
+          padding-right 0
+          padding-top 0
+          height 50px
+          margin 0!important
+          font-size 16px
+          background url('../../assets/images/icons.png') -664px -904px no-repeat
+
+    li.m-i.m-i-live
+      margin 0 16px 0 10px
+
+      a.i-link
+        margin 0
+
+        em
+          padding-left 22px
+          padding-right 0
+          padding-top 0
+          height 50px
+          margin 0!important
+          background url('../../assets/images/icons.png') -664px -518px no-repeat
+          font-size 16px
+
+    li.m-i.m-i-blackroom
+      margin 0 16px 0 10px
+    
+      a.i-link
+          margin 0
+
+          em
+            padding-left 22px
+            padding-right 0
+            padding-top 0
+            height 50px
+            margin 0!important
+            font-size 16px
+
+            &::before
+              position absolute
+              background: url('../../assets/images/icons.png') -727px -519px no-repeat;
+              display inline-block
+              width 22px
+              height 50px
+              left 0
+              content ''
+
+    .home
+      width auto
+      margin 0
+      padding 0 9px 0 0
+
+      a.i-link
+        padding 0
+        background url('../../assets/images/icons.png') -658px -1170px no-repeat
+
+        em
+          padding-right: 0;
+          padding-left: 0;
+          margin: 0;
+      
+    .i_num
+      position absolute
+      display none
+      z-index 100
+      left 0
+      overflow hidden
+      top 49px
+      background #fff
+      border 1px solid #e5e9ef \9
+      border-top 0
+      box-shadow rgba(0,0,0,0.16) 0 2px 4px
+      border-radius 0 0 4px 4px
+      height auto
+
+      li
+        position relative
+        font-size 12px
+        line-height 20px
+        min-width 120px
+        height auto
+        overflow hidden
+        text-align left
+        transition .2s all
+
+        &:hover
+          background-color #e5e9ef
+
+          a
+            left 5px
+            text-shadow none
+            color #222
+
+            em
+              right -21px
+              opacity 1
+
+        a
+          padding 5px 15px 5px 25px
+          margin-right 10px
+          background url(../../assets/images/icons2.png) no-repeat 12px -1613px
+          white-space nowrap
+          transition .2s all
+          overflow hidden
+          position relative
+          left 0
+          color #222
+
+          b
+            display inline-block
+            position relative
+            font-weight normal
+          
+          em
+            background url(../../assets/images/icons2.png) no-repeat 0 -1581px
+            width 15px
+            height 18px
+            display block
+            position absolute
+            right -100px
+            top 0
+            transition .2s all
+            opacity 0
+</style>
--- src/components/common/Num.vue
+++ src/components/common/Num.vue
@@ -2,20 +2,96 @@
   <div class="num">
     <div class="menu-wrapper">
       <Search></Search>
-      <NavMenu></NavMenu>
+      <ul class="nav-menu">
+        <NavMenuItem v-for="i in classify" :item="i" :key="i.title"></NavMenuItem>
+      </ul>
     </div>
   </div>
 </template>
 
 <script>
 import Search from './Search.vue';
-import NavMenu from './NavMenu.vue';
+import NavMenuItem from './NavMenuItem.vue';
 
 export default {
-  
+
+  data() {
+    return {
+      classify: [
+        {
+          title: '首页',
+          num: 0,
+          home: true
+        },
+        {
+          title: '动画',
+          num: 468
+        },
+        {
+          title: '番剧',
+          num: 61
+        },
+        {
+          title: '音乐',
+          num: 749
+        },
+        {
+          title: '舞蹈',
+          num: 108
+        },
+        {
+          title: '游戏',
+          num: 999
+        },
+        {
+          title: '科技',
+          num: 482
+        },
+        {
+          title: '生活',
+          num: 988
+        },
+        {
+          title: '鬼畜',
+          num: 64
+        },
+        {
+          title: '时尚',
+          num: 177
+        },
+        {
+          title: '广告',
+          num: 43
+        },
+        {
+          title: '娱乐',
+          num: 839
+        },
+        {
+          title: '影视',
+          num: 702
+        },
+        {
+          title: '广场',
+          num: 0,
+          sequare: true
+        },
+        {
+          title: '直播',
+          num: 0,
+          live: true
+        },
+        {
+          title: '小黑屋',
+          num: 0,
+          blackroom: true
+        }
+      ]
+    }
+  },
   components: {
     Search,
-    NavMenu
+    NavMenuItem
   }
 }
 </script>
